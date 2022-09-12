62cf12c4441a37278237a9a684214e318f826023
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2022-09-12 13:43:41 +0530

    initial page with the loading animation

---
+++ src/components/loading.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+
+// css
+import "./loading.css";
+
+export default function Loading() {
+  return (
+    <div class="container">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        width="1920"
+        height="1080"
+        viewBox="0 0 1920 1080"
+        id="logo"
+      >
+        <defs>
+          <clipPath id="clip-Lithika_Damnod_Logo">
+            <rect width="1920" height="1080" />
+          </clipPath>
+        </defs>
+        <g
+          id="Lithika_Damnod_Logo"
+          data-name="Lithika Damnod Logo"
+          clip-path="url(#clip-Lithika_Damnod_Logo)"
+        >
+          <g id="logo-frame">
+            <line
+              id="Line_1"
+              data-name="Line 1"
+              y1="293"
+              x2="166"
+              transform="translate(506.5 247.5)"
+              fill="none"
+              stroke="#64ffda"
+              stroke-linecap="round"
+              stroke-width="13"
+            />
+            <line
+              id="Line_2"
+              data-name="Line 2"
+              x1="473"
+              y2="37"
+              transform="translate(672.5 207.5)"
+              fill="none"
+              stroke="#64ffda"
+              stroke-linecap="round"
+              stroke-width="10"
+            />
+            <line
+              id="Line_3"
+              data-name="Line 3"
+              x1="226"
+              y1="389"
+              transform="translate(1145.5 210.5)"
+              fill="none"
+              stroke="#64ffda"
+              stroke-linecap="round"
+              stroke-width="13"
+            />
+            <path
+              id="Path_1"
+              data-name="Path 1"
+              d="M0,351.055,411,0"
+              transform="translate(960 599.5)"
+              fill="none"
+              stroke="#64ffda"
+              stroke-linecap="round"
+              stroke-width="13"
+            />
+            <path
+              id="Path_2"
+              data-name="Path 2"
+              d="M453.5,410.055,0,0"
+              transform="translate(506.5 540.5)"
+              fill="none"
+              stroke="#64ffda"
+              stroke-linecap="round"
+              stroke-width="13"
+            />
+          </g>
+          <g
+            id="Component_1_1"
+            data-name="Component 1 – 1"
+            transform="translate(797.288 378.904)"
+          >
+            <text
+              id="L"
+              transform="translate(0 194)"
+              fill="#64ffda"
+              font-size="220"
+              font-family="YuKyo_Yoko-Bold, YuKyokasho Yoko"
+              font-weight="700"
+            >
+              <tspan x="0" y="0">
+                L
+              </tspan>
+            </text>
+          </g>
+          <g
+            id="Component_2_1"
+            data-name="Component 2 – 1"
+            transform="translate(894.863 440.713)"
+          >
+            <text
+              id="D"
+              transform="translate(7.602 185.523) rotate(17)"
+              fill="#64ffda"
+              font-size="220"
+              font-family="YuKyo_Yoko-Bold, YuKyokasho Yoko"
+              font-weight="700"
+            >
+              <tspan x="0" y="0">
+                D
+              </tspan>
+            </text>
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
+}
